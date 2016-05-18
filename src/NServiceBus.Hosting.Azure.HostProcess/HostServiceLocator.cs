e18a10134f9a897ff2245b23ea9519a79a3fb8bd
Marcin Hoppe <marcin.hoppe@particular.net>
2016-05-18 13:29:33 +0200

    Internalize types in the HostProcess assembly

--- src/NServiceBus.Hosting.Azure.HostProcess/HostServiceLocator.cs
+++ src/NServiceBus.Hosting.Azure.HostProcess/HostServiceLocator.cs
@@ -5,22 +5,10 @@ namespace NServiceBus.Hosting.Azure.HostProcess
     using System.Collections.Generic;
     using Microsoft.Practices.ServiceLocation;
 
-    /// <summary>
-    /// Plugs into the generic service locator to return an instance of <see cref="GenericHost"/>.
-    /// </summary>
-    public class HostServiceLocator : ServiceLocatorImplBase
+    class HostServiceLocator : ServiceLocatorImplBase
     {
-        /// <summary>
-        /// Command line arguments.
-        /// </summary>
         public static string[] Args;
 
-        /// <summary>
-        /// Returns an instance of <see cref="GenericHost"/>
-        /// </summary>
-        /// <param name="serviceType"></param>
-        /// <param name="key"></param>
-        /// <returns></returns>
         protected override object DoGetInstance(Type serviceType, string key)
         {
             var endpoint = Type.GetType(key,true);
@@ -31,11 +19,6 @@ namespace NServiceBus.Hosting.Azure.HostProcess
             return new WindowsHost(endpoint, Args, scannableAssembliesFullName);
         }
 
-        /// <summary>
-        /// Not implemented.
-        /// </summary>
-        /// <param name="serviceType"></param>
-        /// <returns></returns>
         protected override IEnumerable<object> DoGetAllInstances(Type serviceType)
         {
             throw new NotImplementedException();
