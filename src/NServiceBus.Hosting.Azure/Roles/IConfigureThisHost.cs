8ba8c9887612478c15411135354b398bbd20dfd0
Marcin Hoppe <marcin.hoppe@particular.net>
2016-05-24 13:52:28 +0200

    Improve wording on IConfigureThisHost

--- src/NServiceBus.Hosting.Azure/Roles/IConfigureThisHost.cs
+++ src/NServiceBus.Hosting.Azure/Roles/IConfigureThisHost.cs
@@ -1,7 +1,8 @@
 namespace NServiceBus
 {
     /// <summary>
-    /// Indicates that the implementing class will configure the host.
+    /// Indicates that the implementing class will configure the host to dynamically
+    /// load and run multiple endpoints from Azure cloud storage.
     /// </summary>
     public interface IConfigureThisHost
     {
