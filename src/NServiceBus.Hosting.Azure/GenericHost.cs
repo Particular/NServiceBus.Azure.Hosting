f06ad7992c2f501da2207c59ef67f21067782df4
Marcin Hoppe <marcin.hoppe@particular.net>
2016-05-25 14:38:01 +0200

    Host display name uses role instance id

--- src/NServiceBus.Hosting.Azure/GenericHost.cs
+++ src/NServiceBus.Hosting.Azure/GenericHost.cs
@@ -1,8 +1,6 @@
 namespace NServiceBus.Hosting.Azure
 {
     using System;
-    using System.Security.Cryptography;
-    using System.Text;
     using System.Threading;
     using System.Threading.Tasks;
     using Logging;
@@ -61,11 +59,10 @@ namespace NServiceBus.Hosting.Azure
 
                 var host = SafeRoleEnvironment.CurrentRoleName;
                 var instance = SafeRoleEnvironment.CurrentRoleInstanceId;
-                var hostId = DeterministicGuid(instance, host);
                 configuration
                     .UniquelyIdentifyRunningInstance()
-                    .UsingCustomIdentifier(hostId)
-                    .UsingNames(instance, host);
+                    .UsingNames(instance, host)
+                    .UsingCustomDisplayName(instance);
             }
 
             moreConfiguration?.Invoke(configuration);
@@ -87,18 +84,6 @@ namespace NServiceBus.Hosting.Azure
             Environment.FailFast(message, context.Exception);
 
             return Task.FromResult(0);
-        }
-
-        static Guid DeterministicGuid(params object[] data)
-        {
-            // use MD5 hash to get a 16-byte hash of the string
-            using (var provider = new MD5CryptoServiceProvider())
-            {
-                var inputBytes = Encoding.Default.GetBytes(String.Concat(data));
-                var hashBytes = provider.ComputeHash(inputBytes);
-                // generate a guid from the hash:
-                return new Guid(hashBytes);
-            }
         }
 
         IEndpointInstance bus;
