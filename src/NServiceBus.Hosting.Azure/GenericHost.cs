21a9e91ce00cf937ce68a7b806fc7c3b1779f275
Marcin Hoppe <marcin.hoppe@particular.net>
2016-05-27 10:26:41 +0200

    Host display name uses role instance id (#53)
    * Host display name uses role instance id
    
    * display name to include role name and instance id

--- src/NServiceBus.Hosting.Azure/GenericHost.cs
+++ src/NServiceBus.Hosting.Azure/GenericHost.cs
@@ -1,8 +1,6 @@
 namespace NServiceBus.Hosting.Azure
 {
     using System;
-    using System.Security.Cryptography;
-    using System.Text;
     using System.Threading;
     using System.Threading.Tasks;
     using Logging;
@@ -47,7 +45,7 @@ namespace NServiceBus.Hosting.Azure
             PerformConfiguration(builder => builder.EnableInstallers(username)).GetAwaiter().GetResult();
         }
 
-        async Task<IStartableEndpoint> PerformConfiguration(Action<EndpointConfiguration> moreConfiguration = null)
+        Task<IStartableEndpoint> PerformConfiguration(Action<EndpointConfiguration> moreConfiguration = null)
         {
             var configuration = new EndpointConfiguration(endpointNameToUse);
             configuration.DefineCriticalErrorAction(OnCriticalError);
@@ -61,17 +59,17 @@ namespace NServiceBus.Hosting.Azure
 
                 var host = SafeRoleEnvironment.CurrentRoleName;
                 var instance = SafeRoleEnvironment.CurrentRoleInstanceId;
-                var hostId = DeterministicGuid(instance, host);
+                var displayName = $"{host}_{instance}";
                 configuration
                     .UniquelyIdentifyRunningInstance()
-                    .UsingCustomIdentifier(hostId)
-                    .UsingNames(instance, host);
+                    .UsingNames(instance, host)
+                    .UsingCustomDisplayName(displayName);
             }
 
             moreConfiguration?.Invoke(configuration);
 
             specifier.Customize(configuration);
-            return await Endpoint.Create(configuration).ConfigureAwait(false);
+            return Endpoint.Create(configuration);
         }
 
         // Windows hosting behavior when critical error occurs is suicide.
@@ -87,18 +85,6 @@ namespace NServiceBus.Hosting.Azure
             Environment.FailFast(message, context.Exception);
 
             return Task.FromResult(0);
-        }
-
-        static Guid DeterministicGuid(params object[] data)
-        {
-            // use MD5 hash to get a 16-byte hash of the string
-            using (var provider = new MD5CryptoServiceProvider())
-            {
-                var inputBytes = Encoding.Default.GetBytes(String.Concat(data));
-                var hashBytes = provider.ComputeHash(inputBytes);
-                // generate a guid from the hash:
-                return new Guid(hashBytes);
-            }
         }
 
         IEndpointInstance bus;
