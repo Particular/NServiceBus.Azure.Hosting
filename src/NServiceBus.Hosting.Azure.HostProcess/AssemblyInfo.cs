b6eedb9b334bde2d59fae348c82462f76ae06439
Brandon Ording <bording@gmail.com>
2017-11-01 15:31:27 -0400

    Clean up project files

--- src/NServiceBus.Hosting.Azure.HostProcess/AssemblyInfo.cs
+++ src/NServiceBus.Hosting.Azure.HostProcess/AssemblyInfo.cs
@@ -1,10 +1,4 @@
-﻿using System.Reflection;
-using System.Runtime.InteropServices;
+﻿using System.Runtime.InteropServices;
 
-[assembly: AssemblyTitle("NServiceBus.Hosting.Azure.HostProcess")]
-[assembly: AssemblyCopyright("Copyright NServiceBus. All rights reserved")]
-[assembly: AssemblyProduct("NServiceBus.Hosting.Azure.HostProcess")]
-[assembly: AssemblyCompany("NServiceBus Ltd.")]
-[assembly: AssemblyConfiguration("release")]
 [assembly: ComVisible(false)]
 
